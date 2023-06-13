b50ffba27c28d342ae7d827e14c314b356199c22
siri.gangavarapu <sirigangavarapu9@gmail.com>
2023-06-13 17:22:14 -0400

    commit

--- new.py
+++ new.py
@@ -1,3 +1,19 @@
-import os
+import csv
 
-print("My new repo")
+# Dummy data
+data = [
+    {"Name": "John Doe", "Age": 25, "City": "New York"},
+    {"Name": "Jane Smith", "Age": 30, "City": "London"},
+    {"Name": "Michael Johnson", "Age": 35, "City": "Paris"},
+]
+
+# CSV file path
+csv_file = "dummy_data.csv"
+
+# Write data to CSV file
+with open(csv_file, mode="w", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames=["Name", "Age", "City"])
+    writer.writeheader()
+    writer.writerows(data)
+
+print(f"Dummy CSV file '{csv_file}' generated successfully.")
